62f05b59d9cc26905b5da63aae11a7f50599b373
caiooliveira71 <caio.e.oliveira@ba.estudante.senai.br>
2026-03-11 13:37:05 -0300

    Update QUESTÃO-10.py

--- QUESTÃO-10.py
+++ QUESTÃO-10.py
@@ -6,17 +6,22 @@ valor2 = litros * 3.79
 
 if litros <= 25 and combustivel == "A":
     print("desconto de 2%")
-    valor_desconto = valor2 - 2/100
+    desconto = valor2 * 2/100
+    valor_desconto = valor2 - desconto
     print(f"Valor total com o desconto: {valor_desconto}")
 if litros > 25 and combustivel == "A":
     print("desconto de 4%")
-    valor_desconto = valor2 - 4/100
+    desconto = valor2 * 4/100
+    valor_desconto = valor2 - desconto
     print(f"Valor total com o desconto: {valor_desconto}")
 if litros <= 25 and combustivel == "G":
     print("desconto de 3%")
-    valor_desconto = valor1 - 3/100
+    desconto = valor1 * 3/100
+    valor_desconto = valor1 - desconto
     print(f"Valor total com o desconto: {valor_desconto}")
 if litros > 25 and combustivel == "G":
     print("desconto de 5%")
-    valor_desconto = valor1 - 5/100
+    desconto = valor1 * 5/100
+    valor_desconto = valor1 - desconto
+
     print(f"Valor total com o desconto: {valor_desconto}")
